1df189e1463fdbff6c7d1f7df9c2ae3ec2b6969a
Rumsha <rumsha7@gmail.com>
2019-04-09 03:25:02 -0400

    Pyunit testing for exact matching algo: added test cases with * symbol

--- thompsons_nfa/testRegex.py
+++ thompsons_nfa/testRegex.py
@@ -29,10 +29,56 @@ class testRegex(unittest.TestCase):
         self.assertFalse(nfa.match(s))
 
     ## @brief Test a basic regexp with no symbols (only the alphabet)
-    #  @details This is the most straightforward case where the string matches the regexp exactly.
+    #  @details The regexp and string consist of the same alphabet but in a different ordering
     def test_alphabet3(self):
         r = "ab"
-        s = "ab"
+        s = "ba"
+        nfa = compile(r)
+        self.assertFalse(nfa.match(s))
+
+    ## @brief Test a basic regexp with no symbols (only the alphabet)
+    #  @details The regexp and string consist of the same alphabet but of different legnths
+    #           In this case, len(regexp) > len(s)
+    def test_alphabet4(self):
+        r = "ab"
+        s = "a"
+        nfa = compile(r)
+        self.assertFalse(nfa.match(s))
+        
+    ## @brief Test a basic regexp with no symbols (only the alphabet)
+    #  @details The regexp and string consist of the same alphabet but of different legnths
+    #           In this case, len(regexp) < len(s)
+    def test_alphabet5(self):
+        r = "ab"
+        s = "aba"
+        nfa = compile(r)
+        self.assertFalse(nfa.match(s))
+        
+
+    ''' -------------------------------------------------- '''
+    ''' Testing regex with the * symbol'''
+    
+    ## @brief Test a regexp with the * symbol and a 2-letter alphabet
+    #  @details The first character in the regexp contains a * only and the string matches the first half only
+    def test_star1(self):
+        r = "a*b"
+        s = "a"
+        nfa = compile(r)
+        self.assertFalse(nfa.match(s))
+
+    ## @brief Test a regexp with the * symbol and a 2-letter alphabet
+    #  @details The first character in the regexp contains a * only and the string matches the second half only
+    def test_star2(self):
+        r = "a*b"
+        s = "b"
+        nfa = compile(r)
+        self.assertTrue(nfa.match(s))
+
+    ## @brief Test a regexp with the * symbol and a 2-letter alphabet
+    #  @details The first character in the regexp contains a * only and the string matches the entire regexp
+    def test_star3(self):
+        r = "a*b"
+        s = "aaaaaaab"
         nfa = compile(r)
         self.assertTrue(nfa.match(s))
 
